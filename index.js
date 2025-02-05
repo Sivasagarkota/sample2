808b8d857ac7accd595671b9484665ec8d1a5c9e
Sivasagarkota <163895569+Sivasagarkota@users.noreply.github.com>
2025-02-05 15:27:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Hello 1");
 console.log("feature1");
 console.log("feature2");
 console.log("feature3");
-
+console.log("line 6");
 console.log("asdf");
 console.log("update1");
 console.log("update2");
