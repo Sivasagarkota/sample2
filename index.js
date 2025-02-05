b131be1cac13f83aa9c6cf80caedfa9324673252
sagar <as@gmail.com>
2025-02-05 13:14:04 +0530

    added features and updates

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello 1");
+
+console.log("feature1");
+console.log("feature2");
+console.log("feature3");
+
+
+console.log("update1");
+console.log("update2");
+console.log("update3");
